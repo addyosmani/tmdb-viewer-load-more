9c79e2f4404eb82af6f8dc5eeff691c399d26b7b
scarletks1214 <scarlet.ks1214@gmail.com>
2019-03-21 19:59:40 +0300

    fixed loadmore button position, refactoring code, scrollTo newly loaded movie

--- src/components/InfiniteMoviesList.js
+++ src/components/InfiniteMoviesList.js
@@ -1,14 +1,13 @@
-import Grid from '@material-ui/core/Grid';
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import PropTypes from 'prop-types';
-import React from 'react';
-import { FixedSizeList as List } from 'react-window';
-// import {InfiniteLoader, List, WindowScroller, AutoSizer} from 'react-virtualized';
-// import { WindowScroller, AutoSizer } from "react-virtualized";
-import MovieCard from '../containers/MovieCard';
 
+import React, { PureComponent, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { FixedSizeList as List } from 'react-window';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+
+import MovieCard from '../containers/MovieCard';
 
 const ITEM_WIDTH = 400;
 const ITEM_HEIGHT = 360;
@@ -50,7 +49,7 @@ function getMaxItemsAmountPerRow(width) {
 function getRowsAmount(width, itemsAmount, hasMore) {
   const maxItemsPerRow = getMaxItemsAmountPerRow(width);
 
-  return Math.ceil(itemsAmount/ maxItemsPerRow) + (hasMore ? 1 : 0);
+  return Math.ceil(itemsAmount/ maxItemsPerRow);
 }
 
 const RowItem = React.memo(function RowItem({movieId, classes}) {
@@ -61,16 +60,23 @@ const RowItem = React.memo(function RowItem({movieId, classes}) {
   );
 });
 
-
-
-class InfiniteMoviesList extends React.PureComponent {
-
-  loadMoreItems = () => {
+class InfiniteMoviesList extends PureComponent {
+  constructor(props) {
+    super(props);
+    this._listWidth = 0.85 * window.innerWidth;
+    this._listHeight = window.innerHeight - 132;
+    this._listRef = React.createRef();
+  }
+  
+  loadMoreItems = async () => {
     if (!this.props.isFetching) {
-      this.props.fetchMovies();
+      await this.props.fetchMovies();
+      const { movies, hasMore } = this.props;
+      const rowCount = getRowsAmount(this._listWidth, movies.length, hasMore);
+      this._listRef.current.scrollToItem(rowCount, "center");
     }
   };
-
+  
   noItemsRenderer = () => (
     <Grid item>
       <Typography>No movies found</Typography>
@@ -90,51 +96,46 @@ class InfiniteMoviesList extends React.PureComponent {
   }
 
   render() {
-    const {classes} = this.props;
-    const width = 0.85 * window.innerWidth;
-    const height = window.innerHeight;
-    const {movies, hasMore} = this.props;
-    const rowCount = getRowsAmount(width, movies.length, hasMore);
+    const { classes, movies, hasMore } = this.props;
+
+    const rowCount = getRowsAmount(this._listWidth, movies.length, hasMore);
     const loadMoreButton = (
-      hasMore ?
       <Button
         className={classes.button}
         variant="contained"
         color="primary"
         onClick={this.loadMoreItems}
-        style={{marginBottom: '10px'}}
-        fullWidth >
+        style={{ marginBottom: '8px' }}
+        fullWidth>
         Load More
-      </Button> : null
+      </Button>
     );
 
-    const rowRenderer = ({index, style}) => {
-      const {movies, classes} = this.props;
-      const maxItemsPerRow = getMaxItemsAmountPerRow(width);
+    const rowRenderer = ({ index, style }) => {
+      const { movies, classes } = this.props;
+
+      const maxItemsPerRow = getMaxItemsAmountPerRow(this._listWidth);
       const moviesIds = generateIndexesForRow(index, maxItemsPerRow, movies.length).map(movieIndex => movies[movieIndex]);
-    
       return (
         <div style={style} className={classes.row}>
-          {moviesIds.map(movieId => (
-            <RowItem key={movieId} movieId={movieId} classes={classes}/>
-          ))}
+          { moviesIds.map(movieId => (<RowItem key={movieId} movieId={movieId} classes={classes} />)) }
         </div>
       )
     };
 
     return (
-      <React.Fragment>
+      <Fragment>
         <List
+          ref={this._listRef}
           className={classes.grid}
-          height={height}
-          width={width}
+          height={this._listHeight}
+          width={this._listWidth}
           itemCount={rowCount}
-          itemSize={ITEM_HEIGHT}
-        >
+          itemSize={ITEM_HEIGHT}>
           {rowRenderer}
         </List>
-        {loadMoreButton}
-      </React.Fragment>
+        {hasMore && loadMoreButton}
+      </Fragment>
     );
   }
 };
